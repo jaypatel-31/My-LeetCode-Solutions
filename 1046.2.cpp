8ebebeb3efb07b7749fd5e0d30d82186916a1d45
jaypatel-31 <54065115+jaypatel-31@users.noreply.github.com>
2022-04-07 20:42:16 +0530

    1046.2

---
+++ 1046.2.cpp
@@ -0,0 +1,25 @@
+// 1046. Last Stone Weight
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while(stones.size()>1){
+            
+            int n = stones.size();
+            sort(stones.begin(),stones.end());
+            vector<int>::iterator it1,it2;
+            it1 = stones.begin();
+            it2 = stones.end();
+            stones.erase(it1+n-2, it2);
+            
+            if(stones[n-1]==stones[n-2]){
+                
+            }
+            else{
+                int d = stones[n-1]-stones[n-2];
+                stones.push_back(d);
+            }
+    
+        }
+        return stones.size() == 0 ? 0 : stones[0]; 
+    }
+};
